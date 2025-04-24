6331d0aea5d8b9b5463e643db3da98de7889d649
Jocelyn Llamas <mjlldlt@hotmail.com>
2025-04-24 15:25:46 -0600

    update repo

---
+++ Controlles/BoardController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using CollaborativeBoardApi.Hubs;
+using CollaborativeBoardApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CollaborativeBoardApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BoardController : ControllerBase
+    {
+        private readonly IHubContext<BoardHub> _hubContext;
+
+        public BoardController(IHubContext<BoardHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        // Ejemplo de endpoint para limpiar el tablero
+        [HttpPost("clear")]
+        public async Task<IActionResult> ClearBoard()
+        {
+            await _hubContext.Clients.All.SendAsync("BoardCleared");
+            return Ok(new { message = "Board cleared successfully" });
+        }
+
+        // Podríamos añadir más endpoints según sea necesario
+    }
+}
